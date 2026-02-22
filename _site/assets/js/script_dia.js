ea9a266bea1883b8f7254aeaf0d1068bb13fbd32
carol <carolina.nunes.110@ufrn.edu.br>
2026-02-22 17:44:58 -0300

    coisas bonitas

--- _site/assets/js/script_dia.js
+++ _site/assets/js/script_dia.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const datasAtivacao = {
-    "primeiroDia": new Date("2025-03-07"), // A partir de 07/03/2025
-    "segundoDia": new Date("2025-03-11"), // A partir de 11/03/2025
-    "terceiroDia": new Date("2025-03-12"), // A partir de 12/03/2025
-    "quartoDia": new Date("2025-03-13"), // A partir de 13/03/2025
-    "quintoDia": new Date("2025-03-14"), // A partir de 14/03/2025
+    "primeiroDia": new Date("2026-02-23"), // A partir de 07/03/2025
+    "segundoDia": new Date("2026-02-24"), // A partir de 11/03/2025
+    "terceiroDia": new Date("2026-02-25"), // A partir de 12/03/2025
+    "quartoDia": new Date("2026-02-26"), // A partir de 13/03/2025
+    "quintoDia": new Date("2026-02-27"), // A partir de 14/03/2025
     "projetoFinalSimplificado": new Date("2025-03-14") // A partir de 14/03/2025
   };
 
@@ -13,8 +13,37 @@ document.addEventListener("DOMContentLoaded", function () {
   Object.keys(datasAtivacao).forEach(id => {
     const dataAtivacao = datasAtivacao[id];
     const link = document.getElementById(id);
+
     if (agora >= dataAtivacao) {
+
       link.classList.remove('inactive');
+
+      const li = link.closest("li");
+      li.classList.remove("inactive");
+      
+      //console.log(id);
+      
+      if (id === "primeiroDia") {
+        
+        li.classList.add("red");
+      }
+
+      else if (id === "segundoDia") {
+        li.classList.add("orange")
+      }
+
+      else if (id === "terceiroDia") {
+        li.classList.add("yellow")
+      }
+
+      else if (id === "quartoDia") {
+        li.classList.add("green")
+      }
+
+      else if (id === "quintoDia") {
+        li.classList.add("blue")
+      }
+
     }
   });
 });
